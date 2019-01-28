b43f799186e8d8ed10fc04e0eea493bfbdeae813
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2019-01-28 21:47:00 -0200

    Bug fixes and responsive  add

--- js/index.js
+++ js/index.js
@@ -9,3 +9,13 @@ function scrollEsquerda(){
 function clearScroll(){
   clearInterval(intervalo);
 };
+
+function scrollDireita1(){
+  intervalo = setInterval(function(){ document.getElementById('scroller1').scrollLeft += 1 }  , 5);
+};
+function scrollEsquerda1(){
+  intervalo = setInterval(function(){ document.getElementById('scroller1').scrollLeft -= 1 }  , 5);
+};
+function clearScroll1(){
+  clearInterval(intervalo);
+};
